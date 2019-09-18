00bf161581c139f2c1c5f56e478fd98e97a7be33
maharoz <mahruj66@gmail.com>
2019-09-18 20:51:56 +0600

    Managing form input focus

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -7,6 +7,7 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreen extends State<EditProductScreen> {
+final _priceFocusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +23,15 @@ class _EditProductScreen extends State<EditProductScreen> {
             TextFormField(
               decoration: InputDecoration(labelText: 'Title',),
               textInputAction: TextInputAction.next,
-              
+              onFieldSubmitted: (_){
+                FocusScope.of(context).requestFocus(_priceFocusNode);
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Price',),
+              textInputAction: TextInputAction.next,
+              keyboardType: TextInputType.number,
+              focusNode: _priceFocusNode,
             ),
           ],
         )),
